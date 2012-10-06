764ef9b9d69f6d5477847fe31ddfbfb488af9d74
olmissen <olmissen@olmissens-MacBook.local>
2012-10-06 22:51:33 +0200

    modified:   functions.php 	modified:   header.php 	modified:   index.php 	modified:   style.css 	category.php 	single.php 	timthumb.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
-    
-
   <div id="main">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div id="<?php echo get_the_ID(); ?>" class="post">
     <h1><?php the_title(); ?></h1>
-    <p><?php the_content(__('(more...)')); ?></p>
-    <div id="post-info"><p><?php the_time('j. F Y') ?></p></div>
+    <?php the_content(__('(more...)')); ?>
+    <div class="post-info"><p><?php the_time('j. F Y') ?></p></div><!--post-info-->
+    </div><!--PostID-->
     <hr class="post-seperator"/>
     <?php endwhile; else: ?>
     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
